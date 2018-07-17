3ad643f5743dfd1e3cdc71cfc7aa18c86974915b
Pete Hinchcliffe <pete.hinchcliffe@googlemail.com>
2018-07-17 14:40:22 +0100

    Adds basic handler for presenting slack user with dialog to request help for a service

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -8,6 +8,37 @@ import (
 	"github.com/skybet/go-helpdesk/wrapper"
 )
 
+func HelpRequest(w http.ResponseWriter, r *http.Request) error {
+	sc, err := slack.SlashCommandParse(r)
+	if err != nil {
+		return fmt.Errorf("Failed to parse slack slash command: %s", err)
+	}
+
+	descriptionElement := slack.DialogTextElement{
+		Type:        "text",
+		Label:       "Help Request Description",
+		Placeholder: "Describe what you would like help with ...",
+		Name:        "HelpRequestDescription",
+	}
+
+	elements := []slack.DialogElement{
+		descriptionElement,
+	}
+
+	dialog := slack.Dialog{
+		CallbackId:     "HelpRequest",
+		Title:          "Request Help",
+		SubmitLabel:    "Create",
+		NotifyOnCancel: true,
+		Elements:       elements,
+	}
+
+	if err := wrapper.OpenDialog(sc.TriggerID, dialog); err != nil {
+		return fmt.Errorf("Failed to open dialog: %s", err)
+	}
+	return nil
+}
+
 // DialogTest is a simple test handler to create a dialog in Slack
 func DialogTest(w http.ResponseWriter, r *http.Request) error {
 	//get_the_formatted_request and from it get the trigger id
