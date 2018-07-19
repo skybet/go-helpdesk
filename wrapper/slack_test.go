3759ed1805aee6fdbbb11fcc00e912e065246b5a
Adam Pointer <adam.pointer@gmx.com>
2018-07-19 13:36:40 +0100

    Fix test

--- wrapper/slack_test.go
+++ wrapper/slack_test.go
@@ -3,7 +3,7 @@ package wrapper
 import "testing"
 
 func TestInit(t *testing.T) {
-	err := Init("", "")
+	_, err := New("", "")
 	if err == nil {
 		t.Errorf("Invalid slack connections did not return an error")
 	}
