ebd5018ab81521239036e365c172f9cb07452a50
Pete Hinchcliffe <pete.hinchcliffe@googlemail.com>
2018-07-23 20:15:16 +0100

    Adds test for HelpCallback handler

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -11,6 +11,19 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+func TestHelpCallback(t *testing.T) {
+	interactionJSON := "{\"type\": \"dialog_submission\",\"submission\": {\"name\": \"Sigourney Dreamweaver\",\"email\": \"sigdre@example.com\",\"phone\": \"+1 800-555-1212\",\"meal\": \"burrito\",\"comment\": \"No sour cream please\",\"team_channel\": \"C0LFFBKPB\",\"who_should_sing\": \"U0MJRG1AL\"},\"callback_id\": \"employee_offsite_1138b\",\"team\": {\"id\": \"T1ABCD2E12\",\"domain\": \"coverbands\"},\"user\": {\"id\": \"W12A3BCDEF\",\"name\": \"dreamweaver\"},\"channel\": {\"id\": \"C1AB2C3DE\",\"name\": \"coverthon-1999\"},\"action_ts\": \"936893340.702759\",\"token\": \"TOKEN\",\"response_url\": \"https://hooks.slack.com/app/T012AB0A1/123456789/JpmK0yzoZDeRiqfeduTBYXWQ\"}"
+	r := httptest.NewRequest("POST", "/slack", nil)
+	w := httptest.NewRecorder()
+	req := &server.Request{Request: r}
+	res := &server.Response{w}
+
+	err := HelpCallback(res, req, interactionJSON)
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+}
+
 func TestHelpRequest(t *testing.T) {
 	mockSlack := &mocks.SlackWrapper{}
 	mockSlack.On("OpenDialog", "ABC123", mock.Anything).Return(nil)
