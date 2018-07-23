751fa4ea3707148954b932f369df9c6be04741e6
Pete Hinchcliffe <pete.hinchcliffe@googlemail.com>
2018-07-23 18:42:19 +0100

    Add handler to run on submission of Help Dialog
    Example handler to log the information a user submits through the
    HelpRequest handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/nlopes/slack"
+	log "github.com/sirupsen/logrus"
 	"github.com/skybet/go-helpdesk/server"
 	"github.com/skybet/go-helpdesk/wrapper"
 )
@@ -13,6 +15,20 @@ var slackWrapper wrapper.SlackWrapper
 // Init initialises any external dependencies
 func Init(sw wrapper.SlackWrapper) {
 	slackWrapper = sw
+}
+
+func HelpCallback(res *server.Response, req *server.Request, ctx interface{}) error {
+	s, ok := ctx.(string)
+	if !ok {
+		return fmt.Errorf("Expected a string to be passed to the handler")
+	}
+	var d *slack.DialogCallback
+	err := json.Unmarshal([]byte(s), &d)
+	if err != nil {
+		return fmt.Errorf("%s", err)
+	}
+	log.Printf("User: '%s' Requested Help: '%s'", d.User.Name, d.Submission["HelpRequestDescription"])
+	return nil
 }
 
 // HelpRequest is a handler that creates a dialog in Slack to capture a
