2168f68991fcdfe7c894c251c783871485732ef6
Rob Iball <robertgiball@gmail.com>
2018-12-07 11:29:33 +0000

    Nlopes slack upgrade

--- wrapper/slack.go
+++ wrapper/slack.go
@@ -1,7 +1,7 @@
 package wrapper
 
 import (
-	"github.com/BeepBoopHQ/go-slackbot"
+	//"github.com/BeepBoopHQ/go-slackbot"
 	"github.com/nlopes/slack"
 
 	"fmt"
@@ -10,20 +10,20 @@ import (
 // SlackWrapper is a interface for Slack to enable test double injection
 type SlackWrapper interface {
 	OpenDialog(triggerID string, dialog slack.Dialog) error
-	SendMessage(message, channel string)
+	//SendMessage(message, channel string)
 }
 
 // Slack is a wrapper around the Slack App and RTM APIs
 type Slack struct {
 	App *slack.Client
-	Bot *slackbot.Bot
+	Bot *slack.Client
 }
 
 // New takes an app and bot token, verifies the connection and
 // returns an initialised Slack struct
 func New(appToken, botToken string) (*Slack, error) {
 	slackApp := slack.New(appToken)
-	slackBot := slackbot.New(botToken)
+	slackBot := slack.New(botToken)
 
 	// Check tokens are valid
 	_, err := slackApp.AuthTest()
@@ -31,7 +31,7 @@ func New(appToken, botToken string) (*Slack, error) {
 		return nil, err
 	}
 
-	if _, err = slackBot.Client.AuthTest(); err != nil {
+	if _, err = slackBot.AuthTest(); err != nil {
 		return nil, err
 	}
 	return &Slack{App: slackApp, Bot: slackBot}, nil
@@ -46,9 +46,32 @@ func (s *Slack) OpenDialog(triggerID string, dialog slack.Dialog) error {
 	}
 	return err
 }
-
-// SendMessage posts a message to Slack that is visible to everyone in the channel
-func (s *Slack) SendMessage(message, channel string) {
-	p := slack.PostMessageParameters{}
-	s.Bot.Client.PostMessage(fmt.Sprintf("#%s", channel), message, p)
-}
+//
+//// SendMessage posts a message to Slack that is visible to everyone in the channel
+//func (c slack.Client) SendMessage(channelID, message string, params slack.PostMessageParameters) {
+//	s.sendMessage(channelID, message, params, []slack.MsgOption{})
+//}
+//
+//func (s *Slack) SendMessageWithAttachments(channelID, message string, params slack.PostMessageParameters, attachments []slack.Attachment) {
+//	var opts []slack.MsgOption
+//	for _, attachment := range attachments {
+//		opts = append(opts, slack.MsgOptionAttachments(attachment))
+//	}
+//	s.sendMessage(channelID, message, params, opts)
+//}
+//
+//func (s *Slack) sendMessage(channelID, message string, params slack.PostMessageParameters, opts []slack.MsgOption) {
+//	opts = append(opts, slack.MsgOptionText(message, true))
+//
+//	if params.AsUser {
+//		opts = append(opts, slack.MsgOptionAsUser(true))
+//		opts = append(opts, slack.MsgOptionUser(params.Username))
+//	}
+//
+//	channelId := fmt.Sprintf("#%s", channelID)
+//	_, _, err := s.Bot.PostMessage(channelId, opts...)
+//	if err != nil {
+//		fmt.Printf("%s\n", err)
+//		return
+//	}
+//}
